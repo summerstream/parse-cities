f32b7144f091d183c5cb41956d3b4b3915f173b9
culnoty <culnoty@outlook.com>
2017-01-02 19:14:14 +0800

    init

---
+++ processCities.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+import json
+provices = {}
+with open('cities','r+') as f:
+    for line in f:
+        number = line.split(' ')[0]
+        province = number[0:2]
+        city = number[2:4]
+        area = number[4:6]
+        name = line.split(' ')[len(line.split(' '))-1].strip().replace('　','')
+        if city == '00':
+            provices[province]={};
+            provices[province]['name'] = name
+            provices[province]['cities'] = {}
+        elif area == '00':
+            provices[province]['cities'][city]={}
+            provices[province]['cities'][city]['name']=name
+            provices[province]['cities'][city]['areas'] = {}
+        else:
+            provices[province]['cities'][city]['areas'][area]=name
+f.close()
+# print(json.dumps(provices,encoding='utf-8',ensure_ascii=False).decode('UTF-8'))
+print(json.dumps(provices['14']['cities']['05']['areas']['25'],encoding='utf-8',ensure_ascii=False).decode('UTF-8'))
