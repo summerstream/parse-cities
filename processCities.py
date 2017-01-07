764d719cd6f0a66f5d2ae7810867e43de4dff37c
culnoty <culnoty@outlook.com>
2017-01-07 11:04:26 +0800

    dump to file

--- processCities.py
+++ processCities.py
@@ -21,3 +21,7 @@ with open('cities','r+') as f:
 f.close()
 # print(json.dumps(provices,encoding='utf-8',ensure_ascii=False).decode('UTF-8'))
 print(json.dumps(provices['14']['cities']['05']['areas']['25'],encoding='utf-8',ensure_ascii=False).decode('UTF-8'))
+
+w = open('cities.js','w')
+w.write(json.dumps(provices,encoding='utf-8',ensure_ascii=False))
+w.close()
