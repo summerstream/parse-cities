c46328e8abd3856a6702d0023ce3bb58e5826785
culnoty <culnoty@outlook.com>
2017-01-08 15:45:41 +0800

    taiwan

--- processCities.py
+++ processCities.py
@@ -20,8 +20,26 @@ with open('cities','r+') as f:
             provices[province]['cities'][city]['areas'][area]=name
 f.close()
 # print(json.dumps(provices,encoding='utf-8',ensure_ascii=False).decode('UTF-8'))
-print(json.dumps(provices['14']['cities']['05']['areas']['25'],encoding='utf-8',ensure_ascii=False).decode('UTF-8'))
-
+# print(json.dumps(provices['14']['cities']['05']['areas']['25'],encoding='utf-8',ensure_ascii=False).decode('UTF-8'))
+#台湾 澳门 香港特殊处理
+# print (json.dumps(provices['71'],encoding='utf-8',ensure_ascii=False).decode('UTF-8'))
+provices['71']['cities']['00'] = {}
+provices['71']['cities']['00']['name'] = '台湾'
+provices['71']['cities']['00']['areas'] = {
+    '00':'台湾'
+}
+# print (json.dumps(provices['71'],encoding='utf-8',ensure_ascii=False).decode('UTF-8'))
+provices['81']['cities']['00'] = {}
+provices['81']['cities']['00']['name'] = '香港特别行政区'
+provices['81']['cities']['00']['areas'] = {
+    '00':'香港特别行政区'
+}
+# print (json.dumps(provices['71'],encoding='utf-8',ensure_ascii=False).decode('UTF-8'))
+provices['82']['cities']['00'] = {}
+provices['82']['cities']['00']['name'] = '澳门特别行政区'
+provices['82']['cities']['00']['areas'] = {
+    '00':'澳门特别行政区'
+}
 w = open('cities.js','w')
 w.write(json.dumps(provices,encoding='utf-8',ensure_ascii=False))
 w.close()
